1f0ecebdc352207592882a7c3a42b4eabd4a46d2
Justin Kim <justin1004@gmail.com>
2021-10-17 21:55:27 +0900

    added tomcat jasper for jsp

--- src/main/java/com/justin/demo/HomeContoller.java
+++ src/main/java/com/justin/demo/HomeContoller.java
@@ -1,10 +1,15 @@
 package com.justin.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeContoller {
+	
+	@RequestMapping("/home")
 	public String home() {
+		System.out.println("hi");
 		return "home.jsp";
 	}
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	I Love Java
 </body>
 </html>
